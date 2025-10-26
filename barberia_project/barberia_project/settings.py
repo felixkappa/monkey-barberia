5008e81aa3c96c9bab82c02a6c5b13b04301527c
felixkappa <118398978+felixkappa@users.noreply.github.com>
2025-10-26 00:32:00 -0600

    Update settings.py

--- barberia_project/barberia_project/settings.py
+++ barberia_project/barberia_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,6 +128,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -164,3 +166,4 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+
