18a0ee6a9d7286750f7b18993d785269bd8d7039
felixkappa <118398978+felixkappa@users.noreply.github.com>
2025-10-26 20:11:21 -0600

    Update settings.py

--- barberia_project/barberia_project/settings.py
+++ barberia_project/barberia_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)+#0dnso$=b0p$cqhya%pc73ata)kx5x#o993=fw2x9864yk^a"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -168,6 +168,7 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+
 
 
 
